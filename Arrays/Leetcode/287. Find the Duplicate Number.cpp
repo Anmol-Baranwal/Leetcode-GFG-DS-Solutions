bac79705896e4af2f4eed3126cf84d70680adcbf
Anmol-Barawnal <anmolbaranwal119@gmail.com>
2023-02-03 01:34:28 +0530

    Find the Duplicate Number

---
+++ Arrays/Leetcode/287. Find the Duplicate Number.cpp
@@ -0,0 +1,30 @@
+/*
+    Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+    
+    Time: 311 ms (Beats 24.89%), Space: 61.1 MB (Beats 99.62%)
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // without modifying: cannot use nums[i] % size
+        // cannot use freq method 
+        // cannot use map in stl
+        
+        // using cycle in linked list
+        
+        int slow= nums[0];
+        int fast= nums[0];
+        do{
+            slow= nums[slow];
+            fast= nums[nums[fast]];
+        }while(slow!=fast);
+    
+        fast=nums[0];
+        while(slow!=fast){
+            slow= nums[slow];
+            fast= nums[fast];
+        }
+        return slow;
+    }
+};
