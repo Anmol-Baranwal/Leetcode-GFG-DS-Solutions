0428a6db806b9ddfec649093ed5394127ed7cf2d
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-02-03 03:36:09 -0800

    fix problem link

--- Arrays/Leetcode/31. Next Permutation.cpp
+++ Arrays/Leetcode/31. Next Permutation.cpp
@@ -1,5 +1,5 @@
 /*
-    Problem Link: https://leetcode.com/problems/find-the-duplicate-number/
+    Problem Link: https://leetcode.com/problems/next-permutation/
     
     Time: 4 ms (Beats 69.69%), Space: 12.1 MB (Beats 75.20%)
 */
