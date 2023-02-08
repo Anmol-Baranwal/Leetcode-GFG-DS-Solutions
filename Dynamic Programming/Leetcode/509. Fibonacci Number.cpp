2fcdbeabcdfd9e0fb61177d00950e380c441c570
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-02-07 20:24:17 -0800

    add Fibonacci Number

---
+++ Dynamic Programming/Leetcode/509. Fibonacci Number.cpp
@@ -0,0 +1,46 @@
+// -----Approach 1: ------------------------------------------------------------
+
+/*
+    Problem Link: https://leetcode.com/problems/shortest-path-in-binary-matrix/
+    
+    Time: 0 ms (Beats 100%), Space: 5.8 MB (Beats 93.76%)
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)    return n;
+        int prev2= 0;
+        int prev= 1;
+        int curr;
+        for(int i=2; i<=n; i++){
+            curr= prev + prev2;
+            prev2= prev;
+            prev= curr;
+        }
+        return curr;
+    }
+};
+
+// -----Approach 2: ------------------------------------------------------------
+
+/*
+    Problem Link: https://leetcode.com/problems/shortest-path-in-binary-matrix/
+    
+    Time: 3 ms (Beats 69.77%), Space: 6.1 MB (Beats 21.41%)
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)    return 0;
+        int prev2= 0;
+        int prev= 1;
+        for(int i=2; i<=n; i++){
+            int curr= prev + prev2;
+            prev2= prev;
+            prev= curr;
+        }
+        return prev;
+    }
+};
